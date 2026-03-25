0b2589e7404702a1d94c20c5b64f91ac2699b381
Polilo-User <akulovdmitrii12@gmail.com>
2026-03-25 14:33:43 +0700

    fx secret_key

--- kettleTest/kettleTest/settings.py
+++ kettleTest/kettleTest/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
+SECRET_KEY = os.environ.get("SECRET_KEY", "your-default-secret-key")
 
 # Настройки для отправки почты через SMTP
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
